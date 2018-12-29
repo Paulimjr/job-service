3287104fce032623a9b5b1eb0b641a6b3fbe06ca
PaulimJr <paulim.jr@hotmail.com>
2018-12-29 10:46:03 -0200

    Refactoring codes about Job Service with SpringBoot

---
+++ src/main/java/br/com/job/service/JobServiceApplication.java
@@ -0,0 +1,11 @@
+package br.com.job.service;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobServiceApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
